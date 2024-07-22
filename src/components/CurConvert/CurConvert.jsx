d459a929f2656c9cb861574aab48ad42e9b7bbb5
hokevin404 <51719081+hokevin404@users.noreply.github.com>
2024-07-21 21:25:07 -0700

    Displays to and from currency

--- src/components/CurConvert/CurConvert.jsx
+++ src/components/CurConvert/CurConvert.jsx
@@ -5,20 +5,47 @@ import './curConvert.css';
 import Selection from '../../pages/Selection';
 
 // Import modules
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 // import axios from 'axios';
 
 function CurConvert({curr}) {
     const [fromCurr, setFromCurr] = useState('');
     const [toCurr, setToCurr] = useState('');
+    const [resData, setResData] = useState('');
+    axios.defaults.baseURL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies`;
 
-    // console.log(props)
+    function handleClick() {
+    }
+
+    useEffect(()=>{
+        if(fromCurr && toCurr) {
+            try {
+                axios
+                    .get(`/${fromCurr}.json`)
+                    .then(res => {
+                        setResData(res.data[fromCurr]);
+                        console.log(resData);
+                    })
+                } catch (error) {
+                console.error(error)
+            }
+        }
+
+    },[fromCurr, toCurr])
 
     return (
         <div className='currContainer'>
             <Selection curr={curr} menu="fromCurr" setSelect={setFromCurr}/>
             <Selection curr={curr} menu="toCurr" setSelect={setToCurr}/>
             <button type='button' onClick={handleClick}> Convert</button>
+
+            {resData && (
+                <div>
+                    <h2>{fromCurr.toLocaleUpperCase()}: {resData.fromCurr}  {resData[fromCurr]}</h2>
+                    <h2>{toCurr.toLocaleUpperCase()} {resData[toCurr]}</h2>
+                </div>
+            )}
         </div>
     )
 }
